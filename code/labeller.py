63deacedbb75b44d150651c0651b45403795ef67
prasdud <prasdudwastaken@gmail.com>
2024-08-19 17:18:51 +0530

    cleaned profanity.csv

--- code/labeller.py
+++ code/labeller.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import csv
-
+import re
 
 def loadProfanityCsv(profCsv):
     with open(profCsv, mode='r') as file:
@@ -10,17 +10,18 @@ def loadProfanityCsv(profCsv):
     return profaneWords
 
 
-profanityCsv = 'profanity-unique-main.csv'
+profanityCsv = '../data/profanity-unique-main.csv'
 profaneWords = loadProfanityCsv(profanityCsv)
 
 
 def labelProfanity(lyric, profaneWords):
     lyricLower = lyric.lower()
-    return 1 if any(word in lyricLower for word in profaneWords) else 0
+    #return 1 if any(word in lyricLower for word in profaneWords) else 0
+    pattern = r'\b(?:' + '|'.join(re.escape(word) for word in profaneWords) + r')\b'
+    return 1 if re.search(pattern, lyricLower) else 0
 
-
-inputCsv = 'data/final-data-unlabelled.csv'
-labelledData = 'data/final-data-labelled.csv'
+inputCsv = '../data/final-data-unlabelled.csv'
+labelledData = '../data/final-data-labelled.csv'
 
 with open(inputCsv, mode='r') as infile, open(labelledData, mode='a', newline='') as outfile:
     reader = csv.reader(infile)
